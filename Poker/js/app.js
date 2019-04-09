c9402dd20eadcd58a6c84853f822eb7afd57983c
Tommie <tommiealcorn@gmail.com>
2019-04-09 16:31:16 -0600

    Deal, Shuffle, & names

--- Poker/js/app.js
+++ Poker/js/app.js
@@ -1,27 +1,23 @@
 
 let cards = [];
-let cardsPlayed = [];
-
 let suits = ['c', 'd', 'h', 's'];
 let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
 
-let myHand = [];
-let meHand = [];
+let p1Hand = []; p1Score = 0;
+let p2Hand = []; p2Score = 0;
 
 shuffle();
 
-deal();
 
-console.log(myHand);
-console.log(meHand);
 
 function shuffle (){
+    cards = [];
     for(let i = 0; i < suits.length; i++) {
         for(let k = 0; k < ranks.length; k++){
             cards.push(ranks[k] + suits[i]);
         };
     };
-    let currentIndex = 52, tempCard = '  ', randomIndex = 0;
+    let currentIndex = cards.length -1, tempCard = '  ', randomIndex = 0;
 
     while(0 !== currentIndex){
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -34,9 +30,21 @@ function shuffle (){
 };
 
 function deal(){
+    if(cards.length <= 16) {
+        shuffle();
+    };
+
     for(let i = 0; i < 5; i++){
 
-        myHand[i] = cards.shift();
-        meHand[i] = cards.shift();
+        p1Hand[i] = cards.shift();
+        p2Hand[i] = cards.shift();
     };
+    console.log(cards);
+    console.log(p1Hand);
+    console.log(p2Hand);
 };
+
+$('#deal').on('click', function(){deal()});
+$('#stand').on('click', function(){stand()});
+$('#take').on('click', function(){take()});
+$('#fold').on('click', function(){fold()});
